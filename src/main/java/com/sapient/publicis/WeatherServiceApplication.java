4d0c01ec2ba95e673f68e3d4d3b6048c350c76ae
lab <labb@rssoftware.co.in>
2021-06-13 20:30:58 +0530

    Adding support for i18n

--- src/main/java/com/sapient/publicis/WeatherServiceApplication.java
+++ src/main/java/com/sapient/publicis/WeatherServiceApplication.java
@@ -1,10 +1,15 @@
 package com.sapient.publicis;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 @EnableCaching
@@ -17,5 +22,19 @@ public class WeatherServiceApplication {
 	@Bean
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		final SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setUseCodeAsDefaultMessage(true);
+		messageSource.setBasenames("messages");
+		return messageSource;
 	}
 }
